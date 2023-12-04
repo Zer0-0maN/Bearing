3e0e5b2340464e3108fe909b35a4e2885a8846cf
Daniil <Daniil@192.168.5.8>
2023-12-04 20:00:53 +0300

    1) Make test file to find Z

--- test.m
+++ test.m
@@ -10,7 +10,7 @@ E = 3.638; sigma = 1.667;
 %Определение границы устойчивости
 
 %Определение границы расчета
-T1 = 0.00628;
+T1 = 314*2*10^-3;
 %Задание начальных параметров
 zero = startFunc2(0);
 f_numeric = double(subs(zero,0));
@@ -18,20 +18,24 @@ x0 = f_numeric(1); dtx0 = f_numeric(2); dt2x0 = f_numeric(3);
 y0 = f_numeric(4); dty0 = f_numeric(5); dt2y0 = f_numeric(6);
 xd0 = f_numeric(7); yd0 = f_numeric(8);
 %Решение уравнение
-func0 = @(t, y) func1(t, y, 0);
-[t0,h0]=ode45(func0,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
+f0 = @(t, y) func1(t, y, 0);
+[t0,h0]=ode45(f0,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
 test0 = h0(:,1);
-func1 = @(t, y) func1(t, y, 10);
-[t1,h1]=ode45(func1,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
+f1 = @(t, y) func1(t, y, 0.04);
+[t1,h1]=ode45(f1,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
 test1 = h1(:,1);
-func2 = @(t, y) func1(t, y, 20);
-[t2,h2]=ode45(func2,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
+f2 = @(t, y) func1(t, y, 0.08);
+[t2,h2]=ode45(f2,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
 test2 = h2(:,1);
+f3 = @(t, y) func1(t, y, 0.1);
+[t3,h3]=ode45(f3,[0,T1],[x0,dtx0,dt2x0,y0,dty0,dt2y0,xd0,yd0]);
+test3 = h3(:,1);
 figure;
 hold on;
 grid on;
 plot(t0,test0,'r-');
 plot(t1,test1,'b-');
 plot(t2,test2,'g-');
+plot(t3,test3,'k-');
 hold off;
 
